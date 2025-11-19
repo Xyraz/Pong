165eeaaab4ce2db6034b57a3d142c3e52a0294df
xyraz <setrodinomof@outlook.com>
2025-11-19 17:18:51 +0100

    loop through the game to check if exit click event happen than close the game

--- pong.py
+++ pong.py
@@ -9,3 +9,10 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pong")
 
 clock = pygame.time.Clock()
+
+
+while True:
+    for event in pygame.event():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
