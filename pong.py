59d862f9d888f58f2d590e2ee5983abba9b93aa5
xyraz <setrodinomof@outlook.com>
2025-11-21 13:26:09 +0100

    create ball object

--- pong.py
+++ pong.py
@@ -1,6 +1,5 @@
 import pygame, sys
 
-pygame.init()
 
 screen_width = 1280
 screen_height = 800
@@ -10,12 +9,14 @@ pygame.display.set_caption("Pong")
 
 clock = pygame.time.Clock()
 
+ball = pygame.Rect(0,0,30,30)
 
 while True:
-    for event in pygame.event():
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
     pygame.display.update()
     clock.tick(60)
+
