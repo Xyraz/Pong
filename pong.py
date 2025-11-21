73abcf5e5e95db0fd254d59142a581a95622a92b
xyraz <setrodinomof@outlook.com>
2025-11-21 15:57:47 +0100

    centered left paddle

--- pong.py
+++ pong.py
@@ -13,7 +13,7 @@ ball = pygame.Rect(0,0,30,30)
 ball.center = (screen_width/2, screen_height/2)
 
 cpu = pygame.Rect(0,0,20,100)
-
+cpu.center = screen_height/2
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
