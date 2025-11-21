aaf60379331566474033d7fa7d9166f033fc893b
xyraz <setrodinomof@outlook.com>
2025-11-21 16:36:50 +0100

    make sure that ball bounce through the screen

--- pong.py
+++ pong.py
@@ -18,12 +18,26 @@ cpu.centery = screen_height/2
 player = pygame.Rect(0,0,20,100)
 player.midright = (screen_width,screen_width/2)
 
+ball_speed_x=6
+ball_speed_y = 6
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+    
+    if ball.bottom >= screen_height or ball.top <= 0:
+        ball_speed_y *= -1
+    if ball.right >= screen_width or ball.left <= 0:
+        ball_speed_x *= -1
+
+    screen.fill('black')
     pygame.draw.aaline(screen,'white',(screen_width/2,0),(screen_width/2,screen_height))
     pygame.draw.ellipse(screen,'white',ball)
     pygame.draw.rect(screen,'white',cpu)
