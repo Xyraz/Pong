5cfdc85cd52d1ff36d366e2aba4009e83607da2e
xyraz <setrodinomof@outlook.com>
2025-11-21 15:38:52 +0100

    make sure object is on screen

--- pong.py
+++ pong.py
@@ -1,15 +1,16 @@
 import pygame, sys
 
-
 screen_width = 1280
 screen_height = 800
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Pong")
 
+
 clock = pygame.time.Clock()
 
 ball = pygame.Rect(0,0,30,30)
+
 
 while True:
     for event in pygame.event.get():
@@ -17,6 +18,7 @@ while True:
             pygame.quit()
             sys.exit()
 
+    pygame.draw.rect(screen,'white',ball)
     pygame.display.update()
     clock.tick(60)
 
